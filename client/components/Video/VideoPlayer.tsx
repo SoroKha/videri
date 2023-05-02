20a8ef23672a5a6e4b357d036ad46577321e4508
SoroKha <soroushkhammar@gmail.com>
2023-05-02 21:32:14 +1000

    Volume mute func.

--- client/components/Video/VideoPlayer.tsx
+++ client/components/Video/VideoPlayer.tsx
@@ -4,6 +4,7 @@ import PauseIcon from '@mui/icons-material/Pause';
 import ReplayIcon from '@mui/icons-material/Replay';
 import LoopIcon from '@mui/icons-material/Loop';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
 import SettingsIcon from '@mui/icons-material/Settings';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
@@ -21,10 +22,13 @@ export default function VideoPlayer() {
   const [fullscreen, setFullscreen] = useState<boolean>(false);
   const [loop, setLoop] = useState<boolean>(false);
   const [volume, setVolume] = useState<number>(0);
+  const [prevVolume, setPrevVolume] = useState<number>(0);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const volumeRef = useRef<HTMLInputElement>(null);
   const progressBarRef = useRef<HTMLInputElement>(null);
+
+
   
 
   useEffect(() => {
@@ -81,14 +85,22 @@ export default function VideoPlayer() {
     if (video && volume) {
       const currentVolume = (volume.valueAsNumber / 100);
       video.volume = currentVolume;
+      setVolume(currentVolume);
     }
   }
 
-  function muteVolume() {
-    const volume = volumeRef.current;
-    
-    if (video && volume) {
-      video.volume = 0;
+  function handleVolumeMute() {
+    if (video) {
+      if (volume === 0) {
+        video.volume = prevVolume;
+        setVolume(video.volume);
+      }
+      else {
+        setPrevVolume(video.volume);
+        video.volume = 0;
+        setVolume(video.volume);
+      }
+
     }
   }
 
@@ -132,44 +144,46 @@ export default function VideoPlayer() {
         controls={false}
         />
         <div className={styles.customControls}>
-            <Button className={styles.loopBtn} onClick={() => setLoop(loop => !loop)}>
-              <LoopIcon sx={{color: loop ? 'green' : null}} />
-            </Button>
-            <Button className={styles.playBtn} onClick={handlePlayPause}>
-              {timeStatus === 'Paused' ? <PlayArrowIcon /> : null}
-              {timeStatus === 'Playing' ? <PauseIcon /> : null}
-              {timeStatus === 'Finished' ? <ReplayIcon /> : null}
-            </Button>
-                <input
-                className={styles.progressBar}
-                type="range"
-                min={0}
-                max={100}
-                step="any"
-                value={(currentTime / duration) * 100}
-                onChange={handleProgressBarChange}
-                ref={progressBarRef}
-                />
-                <Button className={styles.volumeBtn} onClick={muteVolume}>
-        
-                </Button>
-                <input
-                className={styles.volumeBar}
-                type="range"
-                min={0}
-                max={100}
-                defaultValue={50}
-                onChange={handleVolumeChange}
-                ref={volumeRef}
-                />
-                <div className={styles.timeContainer}>
-                    <span className={styles.time}>
-                      {formatTime(currentTime)} / {formatTime(duration)}
-                    </span>                    
-                </div>
-            <Button className={styles.fullscreenBtn} onClick={handleFullscreen} sx={{marginLeft: 'auto', borderRadius: 0}}>
-              {fullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
-            </Button>
+          <Button className={styles.loopBtn} onClick={() => setLoop(loop => !loop)}>
+            <LoopIcon sx={{color: loop ? 'green' : null}} />
+          </Button>
+          <Button className={styles.playBtn} onClick={handlePlayPause}>
+            {timeStatus === 'Paused' && <PlayArrowIcon />}
+            {timeStatus === 'Playing' && <PauseIcon />}
+            {timeStatus === 'Finished' && <ReplayIcon />}
+          </Button>
+          <input
+          className={styles.progressBar}
+          type="range"
+          min={0}
+          max={100}
+          step="any"
+          value={(currentTime / duration) * 100}
+          onChange={handleProgressBarChange}
+          ref={progressBarRef}
+          />
+          <Button className={styles.volumeBtn} onClick={handleVolumeMute}>
+            {volume === 0 && <VolumeOffIcon />}
+            {volume < 0.5 && volume !== 0 && <VolumeDownIcon />}
+            {volume >= 0.5 && <VolumeUpIcon />}
+          </Button>
+          <input
+          className={styles.volumeBar}
+          type="range"
+          min={0}
+          max={100}
+          value={volume * 100}
+          onChange={handleVolumeChange}
+          ref={volumeRef}
+          />
+          <div className={styles.timeContainer}>
+              <span className={styles.time}>
+                {formatTime(currentTime)} / {formatTime(duration)}
+              </span>                    
+          </div>
+          <Button className={styles.fullscreenBtn} onClick={handleFullscreen} sx={{marginLeft: 'auto', borderRadius: 0}}>
+            {fullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+          </Button>
         </div>
     </div>
   );
